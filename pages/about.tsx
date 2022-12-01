ae82d6521d1d9e12612eed09161ae4a5417c73b2
c <thomascavalcante77@gmail.com>
2022-11-30 21:18:01 -0500

    changing about text

--- pages/about.tsx
+++ pages/about.tsx
@@ -86,7 +86,7 @@ export default function Example() {
             <h1>
               <span className="block text-center text-lg font-semibold text-sky-600">Introducing</span>
               <span className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-200 sm:text-4xl">
-                JavaScript for Beginners
+                Quotom: A payment platform to split & share payments.
               </span>
             </h1>
             <p className="mt-8 text-xl leading-8 text-gray-300">
@@ -97,10 +97,10 @@ export default function Example() {
           </div>
           <div className="prose prose-lg prose-indigo mx-auto mt-6 text-gray-300">
             <p>
-              Faucibus commodo massa rhoncus, volutpat. <strong>Dignissim</strong> sed <strong>eget risus enim</strong>.
+              Faucibus commodo massa rhoncus, volutpat. <strong className='text-white'>Dignissim</strong> sed <strong className='text-white'>eget risus enim</strong>.
               Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra
               tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim.{' '}
-              <a href="#">Mattis mauris semper</a> sed amet vitae sed turpis id.
+              <a href="#" className='text-sky-400'>Mattis mauris semper</a> sed amet vitae sed turpis id.
             </p>
             <ul role="list">
               <li>Quis elit egestas venenatis mattis dignissim.</li>
@@ -112,14 +112,14 @@ export default function Example() {
               porttitor ultrices gravida eu. Tincidunt leo, elementum mattis elementum ut nisl, justo, amet, mattis. Nunc
               purus, diam commodo tincidunt turpis. Amet, duis sed elit interdum dignissim.
             </p>
-            <h2>From beginner to expert in 30 days</h2>
+            <h2 className='text-white'>From beginner to expert in 30 days</h2>
             <p>
               Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
               Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus
               mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis
               ipsum eu a sed convallis diam.
             </p>
-            <blockquote>
+            <blockquote className='text-white'>
               <p>
                 Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
                 pellentesque. Blandit amet, sed aenean erat arcu morbi.
@@ -137,9 +137,9 @@ export default function Example() {
                 width={1310}
                 height={873}
               />
-              <figcaption>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
+              <figcaption className='text-white'>Sagittis scelerisque nulla cursus in enim consectetur quam.</figcaption>
             </figure>
-            <h2>Everything you need to get up and running</h2>
+            <h2 className='text-white'>Everything you need to get up and running</h2>
             <p>
               Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>. Amet, massa quam varius orci dapibus
               volutpat cras. In amet eu ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut viverra ridiculus
